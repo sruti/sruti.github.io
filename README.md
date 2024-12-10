43e2d3aa10308a5d106cb0efb00619ceab74883e
sruti <msrutiv@gmail.com>
2024-12-10 21:31:09 +0100

    Website refresh

--- README.md
+++
@@ -1,1 +0,0 @@
-# sruti.github.io
---
+++ js/loadHeaderFooter.js
@@ -0,0 +1,12 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
+
+// Load header and footer
+loadComponent('header', 'header.html');
+loadComponent('footer', 'footer.html');
